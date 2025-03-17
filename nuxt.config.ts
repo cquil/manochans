e1fa0bac7a1aa0b35ae13e10c278da43bf23e110
シ・ク <c.quil155s@gmail.com>
2025-03-17 22:53:51 +0900

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,10 +48,11 @@ export default defineNuxtConfig({
     },
   ],
 
-  modules: ["@nuxt/image", 'nuxt-aos', 'nuxt-microcms-module', '@zadigetvoltaire/nuxt-gtm'],
+  modules: ["@nuxt/image", '@zadigetvoltaire/nuxt-gtm', 'nuxt-aos', 'nuxt-microcms-module'],
 
   gtm: {
-    id: 'GTM-NKWPK4TH'
+    id: 'GTM-NKWPK4TH',
+    enabled: true
   },
 
   // microCMS
