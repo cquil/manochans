5fbaea5ad7685f9888caebcf607852628d1bb71e
シ・ク <c.quil155s@gmail.com>
2025-03-18 23:06:46 +0900

    font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&display=swap' },
+        // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&display=swap' },
         { rel: 'icon', href: '/favicon.ico' }
       ]
     },
