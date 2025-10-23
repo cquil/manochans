9b9c53cc4ab4d5e277c0649e89ed558400008418
シ・ク <c.quil155s@gmail.com>
2025-10-23 22:00:04 +0900

    env編集

--- server/utils/microcms.ts
+++ server/utils/microcms.ts
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const microcmsClient = createClient({
   serviceDomain: 'manochans', // microCMSのサービスドメイン
-  apiKey: "ZcQJV7DcuwfsgLx22aVAWJbR3CNo9fdhoPDk", // APIキー（.env から読み込む）
+  apiKey: process.env.microCMS_API_key, // APIキー（.env から読み込む）
 });
